3a3347a00feb9ea5b3693187df83a7f3aabb3354
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2023-01-16 14:22:42 +0530

    Add files via upload

---
+++ add-example2/AddServlet.java
@@ -0,0 +1,22 @@
+package com.namitha;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int a = Integer.parseInt(req.getParameter("num1"));
+		int b = Integer.parseInt(req.getParameter("num2"));
+		
+		int c = a + b;
+		
+		PrintWriter out = res.getWriter();		
+		out.println("Result: "+c);
+	}
+
+}
